d2f238c646cf3fe8c0226f8e118815e0c4b1398c
Juan <juan.martinez63@myhunter.cuny.edu>
2022-03-11 23:19:34 -0500

    project 5 finished
    all regular user stories submitted

---
+++ assignments/Unit-5-Assignment/Parstagram/Parstagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  postCell.swift
+//  Parstagram
+//
+//  Created by Juan Martinez on 3/11/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
